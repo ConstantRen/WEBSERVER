42a378c9bc273152def1e0929be1862076dc3a6f
ConstantRen <Rmarianoconstantino@gmail.com>
2024-07-27 19:59:45 +0800

    nodejs-lab

---
+++ nodejs-lab/modules/contactModule.js
@@ -0,0 +1,6 @@
+// modules/aboutModule.js
+function aboutContent(name) {
+    return `<h1>About Us</h1><p>We are a company dedicated to serving you. Hello, ${name}!</p>`;
+}
+
+module.exports = aboutContent;
---
+++ nodejs-lab/modules/rootModule.js
@@ -0,0 +1,6 @@
+// modules/rootModule.js
+function rootContent(name) {
+    return `<h1>Welcome to the Home Page</h1><p>Hello, ${name}!</p>`;
+}
+
+module.exports = rootContent;
