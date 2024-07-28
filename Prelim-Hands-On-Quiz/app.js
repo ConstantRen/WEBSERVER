d43ba1656afe71262d677ddb7366ca6e7a9dde70
ConstantRen <rmarianoconstantino@gmail.com>
2024-07-28 19:46:41 +0800

    July 28 2024

---
+++ Prelim-Hands-On-Quiz/app.js
@@ -0,0 +1,24 @@
+// Prelim-Hands-On-Quiz/app.js
+
+const { add, subtract, multiply, divide } = require('./calculation');
+
+const ratePerHour = 300;
+const hoursWorkedPerDay = 4;
+const daysWorkedPerWeek = 6;
+
+const grossIncome = multiply(ratePerHour, multiply(hoursWorkedPerDay, daysWorkedPerWeek));
+const tax = divide(grossIncome, 10);  // 10% tax
+const sss = 1200;
+const pagIbigFund = 300;
+const philHealth = 400;
+
+const totalDeductions = add(add(tax, sss), add(pagIbigFund, philHealth));
+const netSalary = subtract(grossIncome, totalDeductions);
+
+console.log(`The gross income is ${grossIncome}.`);
+console.log(`Tax: ${tax}`);
+console.log(`SSS: ${sss}`);
+console.log(`Pag-Ibig fund: ${pagIbigFund}`);
+console.log(`PhilHealth: ${philHealth}`);
+console.log(`Total deductions: ${totalDeductions}`);
+console.log(`The net salary is ${netSalary}.`);
